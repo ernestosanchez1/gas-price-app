d4718662cf4bf4675d615d003aeb0852273eceb9
Ernesto Sanchez <ejsr13@gmail.com>
2025-03-31 22:08:12 -0700

    go back to normal

--- app.py
+++ app.py
@@ -49,5 +49,5 @@ def compare():
     
     return render_template('index.html')
 
-#if __name__ == '__main__':
-#    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8000)))
+if __name__ == '__main__':
+    app.run()
